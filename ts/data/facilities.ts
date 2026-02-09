428702fe78abf74dd21d771b34dfc029a69b979a
Joshua Thome <joshpthome@gmail.com>
2026-02-09 16:30:35 -0600

    Fix #1

--- ts/data/facilities.ts
+++ ts/data/facilities.ts
@@ -266,8 +266,8 @@ export const facilities: Immutable<Record<FacilityID, Facility>> = {
   },
   [FacilityID.GRINDING_UNIT]: {
     category: FacilityCategory.PRODUCTION_II,
-    width: 3,
-    height: 3,
+    width: 6,
+    height: 4,
     power: 50,
     beltInputs: 'down',
     beltOutputs: 'up',
