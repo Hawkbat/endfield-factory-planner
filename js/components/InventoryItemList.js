0423485a2d282f0283a70583643e488e3945eb55
Joshua Thome <joshpthome@gmail.com>
2026-02-09 15:30:20 -0600

    Updates

--- js/components/InventoryItemList.js
+++
@@ -1,6 +0,0 @@
-import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
-const _jsxFileName = "E:/Projects/endfield-factory-planner/ts/components/InventoryItemList.tsx";
-import { InventoryItem } from "./InventoryItem.js";
-export function InventoryItemList({ itemIDs, currentItemID, onSelectItem }) {
-    return _jsxDEV("div", { className: "inventory-item-list", children: itemIDs.map(itemID => _jsxDEV(InventoryItem, { itemID: itemID, selected: itemID === currentItemID, onSelect: onSelectItem }, itemID, false, { fileName: _jsxFileName, lineNumber: 7, columnNumber: 31 }, this)) }, void 0, false, { fileName: _jsxFileName, lineNumber: 6, columnNumber: 11 }, this);
-}
